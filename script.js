03eaae898c4f2decce3dcc04df29b152bebd7074
Md.Mahedi hasan <mahedicsit@gmail.com>
2020-08-28 18:14:11 +0600

    Second comment with more optimized code

--- script.js
+++ script.js
@@ -23,41 +23,51 @@ function validateEmailFormat(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+// Required field check
+function requiredField(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, "This field is required");
+    } else {
+      successMessage(input);
+    }
+  });
+}
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-
+  requiredField([userName, email, password, repeatPassword]);
   //   validate user input
-  //validate username
-  if (userName.value === "") {
-    showError(userName, "Input field is required");
-  } else {
-    successMessage(userName);
-  }
-  //validate email
-  if (email.value === "") {
-    showError(email, "Input field is required");
-  } else if (!validateEmailFormat(email.value)) {
-    showError(email, "Invalid email address");
-  } else {
-    successMessage(email);
-  }
-  //validate password
-  if (password.value === "") {
-    showError(password, "Input field is required");
-  } else if (password.value.length < 6) {
-    showError(password, "Min 6 character password required");
-  } else {
-    successMessage(password);
-  }
+  //   //validate username
+  //   if (userName.value === "") {
+  //     showError(userName, "Input field is required");
+  //   } else {
+  //     successMessage(userName);
+  //   }
+  //   //validate email
+  //   if (email.value === "") {
+  //     showError(email, "Input field is required");
+  //   } else if (!validateEmailFormat(email.value)) {
+  //     showError(email, "Invalid email address");
+  //   } else {
+  //     successMessage(email);
+  //   }
+  //   //validate password
+  //   if (password.value === "") {
+  //     showError(password, "Input field is required");
+  //   } else if (password.value.length < 6) {
+  //     showError(password, "Min 6 character password required");
+  //   } else {
+  //     successMessage(password);
+  //   }
 
-  if (repeatPassword.value === "") {
-    showError(repeatPassword, "Input field is required");
-  } else if (repeatPassword.value.length < 6) {
-    showError(repeatPassword, "Min 6 character password required");
-  } else if (password.value !== repeatPassword.value) {
-    showError(repeatPassword, "Repeat Password Doesn't Match");
-  } else {
-    successMessage(repeatPassword);
-  }
+  //   if (repeatPassword.value === "") {
+  //     showError(repeatPassword, "Input field is required");
+  //   } else if (repeatPassword.value.length < 6) {
+  //     showError(repeatPassword, "Min 6 character password required");
+  //   } else if (password.value !== repeatPassword.value) {
+  //     showError(repeatPassword, "Repeat Password Doesn't Match");
+  //   } else {
+  //     successMessage(repeatPassword);
+  //   }
 });
